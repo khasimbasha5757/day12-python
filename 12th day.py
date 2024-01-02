d616072373a4217779fe4e288a5664c29cdab752
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2024-01-02 20:08:37 +0530

    Add files via upload

---
+++ 12th day.py
@@ -0,0 +1,48 @@
+# for loops
+name="Khasim basha"
+for i in name:
+         print(i)
+         if(i=="b"):
+                 print("This is something special")
+colors=["apple","Green","Mango"]
+for color in colors:
+          print(color) 
+          for i in color:
+                  print(i)
+
+# how to use range by start step exit
+for i in range(1,12,3):
+          print(i+2 )
+
+
+
+# tables using for loop
+a=int(input("give me a number ,i will give its table : "))
+for i in range(1,11):
+          print( a,"x",i,"=",a*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
